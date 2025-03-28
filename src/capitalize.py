d7f207333c2a4890a2397931c62759c9197eb863
oleg_dixon <olegdixon5973@gmail.com>
2025-03-28 11:22:06 +0500

    add directory src

---
+++ src/capitalize.py
@@ -0,0 +1,8 @@
+text = ''
+
+def capitalize(text):
+    if text == '':
+        return ''
+    first_char = text[0].upper()
+    rest_substring = text[1:]
+    return f'{first_char}{rest_substring}'
