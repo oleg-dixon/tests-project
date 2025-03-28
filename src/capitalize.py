dc748a8919e87d1dd080298700ba9b7608fc18cc
oleg_dixon <olegdixon5973@gmail.com>
2025-03-28 11:24:44 +0500

    deleted capitalize.py

--- src/capitalize.py
+++
@@ -1,8 +0,0 @@
-text = ''
-
-def capitalize(text):
-    if text == '':
-        return ''
-    first_char = text[0].upper()
-    rest_substring = text[1:]
-    return f'{first_char}{rest_substring}'
